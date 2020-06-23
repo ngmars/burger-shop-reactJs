1b2e51700793e9a2cc8c2cd19cbdf5a94b9e197b
Nitish <nitishgopinath@gmail.com>
2020-06-23 19:48:56 +0530

    5th-Commit

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,13 +3,20 @@ import classes from './Burger.css';
 import BurgerIngredients from './BurgerIngredients/BurgerIngredients';
 
 const burger=(props) =>{
+    const transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey =>{
+        return[...Array(props.ingredients[igKey])].map((_,i)=>{
+            return<BurgerIngredients key={igKey +1} type={igKey} />;
+        });
+    }).reduce((arr,el)=>{
+       return arr.concat(el) 
+    },[]);
+    console.log(transformedIngredients)
     return(
         <div className={classes.Burger}>
             <BurgerIngredients type="bread-top"/>
-            <BurgerIngredients type="cheese"/>
-            <BurgerIngredients type="meat"/>
+            {transformedIngredients}
             <BurgerIngredients type="bread-bottom"/>
-
 
         </div>
     );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,7 +5,7 @@ class BurgerBuilder extends Component{
 
     state ={
         ingredients:{
-            salad:1,
+            salad:0,
             bacon:1,
             cheese:2,
             meat:2
